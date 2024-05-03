aa743a62838273a7cfb1afe407ce671c82ccf0a2
atifwattoo <atifwattoo2@gmail.com>
2024-05-04 01:04:29 +0500

    clerk authentication setup done

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://refined-walrus-3.clerk.accounts.dev.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
